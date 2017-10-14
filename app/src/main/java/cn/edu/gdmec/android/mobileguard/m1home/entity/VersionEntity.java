b78cc19b058fbd31d0bdde55d23ac0a0be5aa46f
huangguangda <2397923107@qq.com>
2017-10-14 20:56:34 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/entity/VersionEntity.java
@@ -0,0 +1,14 @@
+package cn.edu.gdmec.android.mobileguard.m1home.entity;
+
+/**
+ * Created by Jack on 2017/10/12.
+ */
+
+public class VersionEntity {
+    /***服务器版本号*/
+    public String versioncode;
+    /***版本描述*/
+    public String description;
+    /***apk下载地址*/
+    public String apkurl;
+}
---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/LostFindActivity.java
@@ -0,0 +1,24 @@
+package cn.edu.gdmec.android.mobileguard.m2theftguard;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import cn.edu.gdmec.android.mobileguard.R;
+
+
+public class LostFindActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_lost_find );
+        startSetup1Activity();
+    }
+
+    private void startSetup1Activity() {
+        Intent intent = new Intent ( LostFindActivity.this,Setup1Activity.class );
+        startActivity ( intent );
+        finish ();
+    }
+}
